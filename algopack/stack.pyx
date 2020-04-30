55122b704c8e7eacd7b403ea7d685786d0c4b9e7
divykjain <divykjain@gmail.com>
2020-04-30 22:42:00 +0530

    Added reference management in stack

--- algopack/stack.pyx
+++ algopack/stack.pyx
@@ -1,24 +1,27 @@
 # distutils: language = c++
 
-from cpython.ref cimport PyObject
+from cpython.ref cimport Py_DECREF, Py_INCREF
 from libcpp.stack cimport stack
 
-ctypedef PyObject* PyObjectPtr
+ctypedef void* VoidPtr
+
 
 cdef class Stack:
-    cdef stack[PyObjectPtr] _stack
+    cdef stack[VoidPtr] _stack
     
     def __cinit__(self):
-        self._stack = stack[PyObjectPtr]()
+        self._stack = stack[VoidPtr]()
 
     cpdef void push(self, object element):
-        self._stack.push(<PyObjectPtr>element)
+        Py_INCREF(<object>element)
+        self._stack.push(<void*>element)
     
     cpdef object pop(self):
         if self._stack.empty():
             raise IndexError("pop from empty stack")
         cdef object popped = <object>self._stack.top()
         self._stack.pop()
+        Py_DECREF(<object>popped)
         return popped
 
     cpdef object top(self):
@@ -27,7 +30,14 @@ cdef class Stack:
         return <object>self._stack.top()
 
     cpdef bint isEmpty(self):
-        return self._stack.empty()
+        return <bint>self._stack.empty()
 
     cpdef size_t size(self):
-        return self._stack.size()
+        return <size_t>self._stack.size()
+
+    def __repr__(self):
+        size = self.size()
+        if size:
+            return f"<Stack size={size} top={repr(self.top())}>"
+        else: 
+            return "<Stack empty>"
